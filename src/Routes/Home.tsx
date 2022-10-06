4d082e5da36be99955433f23ab8016ed25b357c8
reignkk1 <ruaals456@naver.com>
2022-10-06 17:38:20 +0900

    header

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Main = styled.div`
+  height: 200vh;
+  background-color: white;
+`;
+
+function Home() {
+  return <Main></Main>;
+}
+
+export default Home;
